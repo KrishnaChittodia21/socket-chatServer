7aad299f2b3371c6908727c42d74d128a7cde91f
krishna <krishnachittodia.10@gmail.com>
2020-08-18 04:30:41 +0530

    added threads to the list

--- server/chat-server.js
+++ server/chat-server.js
@@ -63,8 +63,10 @@ ws.on('connection', (ws) => {
               });
             }
           });
+          break;
         case 'LOGIN':
           login(parsed.data.email, parsed.data.password);
+          break;
         case 'SEARCH':
           models.User.find({where: {email: {like: parsed.data}}}, (err2, users) => {
             if (!err2 && users) {
@@ -72,10 +74,43 @@ ws.on('connection', (ws) => {
                 type: 'GOT_USERS',
                 data: {
                   users: users,
-                }
+                },
               }));
             }
           });
+          break;
+        case 'FIND_THREAD':
+          models.Thread.findOne({where: {
+            and: [
+              {users: {like: parsed.data[0]}},
+              {users: {like: parsed.data[1]}},
+            ],
+          }}, (err, thread) => {
+            console.log(err);
+            if (!err && thread) {
+              ws.send(JSON.stringify({
+                type: 'ADD_THREAD',
+                data: thread,
+              }));
+            } else {
+              models.Thread.create({
+                lastUpdated: new Date(),
+                users: parsed.data,
+              }, (err2, thread) => {
+                if (!err2 && thread) {
+                  clients.filter(u => thread.users.indexOf(u.id.toString()) > -1).map(client => {
+                    client.ws.send(JSON.stringify({
+                      type: 'ADD_THREAD',
+                      data: thread,
+                    }));
+                  });
+                }
+              });
+            }
+          });
+          break;
+        case 'THREAD_LOAD':
+          
         default:
           console.log('Nothing to see here...');
       }
